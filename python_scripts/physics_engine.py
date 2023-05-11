2e0322bd31051edaded77b5d515d1793e7168af3
mhdbabayi <m.babayi-semiromi@cranfield.ac.uk>
2023-05-11 11:56:55 +0100

    started chrono style physics engine, won't develop it too much

--- python_scripts/physics_engine.py
+++ python_scripts/physics_engine.py
@@ -1,14 +1,68 @@
 import numpy as np
 import flexring as flx
+from enum import Enum
+class ConstraintType(Enum):
+    '''
+    Name of constraint referes to the axis that's not updated 
+    in the update method. The axis can still be explicitly changed 
+    '''
+    X = 1 << 0
+    Y= 1 << 1
+    THETA = 1 <<3
+    
 
 class DynamicObject:
     simParameters = {
-    "universalTime" : 0,
-    "timeStep" : 0.01}
+    "universal_time" : 0,
+    "time_step" : 0.01}
     def initialize(self):
         pass
     def iterate(self):
         pass
+class RigidBody(DynamicObject):
+    def __init__(self,
+                mass,
+                initial_x,
+                initial_y,
+                initial_x_dot,
+                initial_y_dot,
+                constraintType:str,
+                initial_force_y = 0,
+                initial_force_x = 0) -> None:
+        self.states = {'x', initial_x, 'y', initial_y,
+                        'x_dot', initial_x_dot,'y_dot',initial_y_dot,
+                        'x_dot_dot',0, 'y_dot_dot', 0}
+        self.forces = {'x', initial_force_x, 'y', initial_force_y, 'theta', 0}
+        self.mass = mass
+        '''
+        the constraint is a string with three digits, each representing a bit
+        from left to right, Theta, Y, X
+        for example, '001' means, object is constrained in the X direction, and theta and Y are updated in the dynamics 
+        '''
+        self.constraint = constraintType
+    def update_states(self):
+        self.states['x_dot']  = self.states['x_dot'] + self.states['x_dot_dot'] * self.simParameters['time_step']
+        self.states['x']  = self.states['x'] + self.states['x_dot'] * self.simParameters['time_step']
+
+        self.states['y_dot']  = self.states['y_dot'] + self.states['y_dot_dot'] * self.simParameters['time_step']
+        self.states['y']  = self.states['y'] + self.states['y_dot'] * self.simParameters['time_step']
+    def update_accelerations(self):
+        if ~(int(self.constraint) & ConstraintType.X):
+            self.states['x_dot_dot'] = self.forces['x'] / self.mass
+        if ~(int(self.constraint) & ConstraintType.Y):
+            self.states['y_dot_dot'] = self.forces['y']
+    def update_forces(self):
+        raise NotImplementedError
+    def iterate(self):
+        self.update_forces()
+        self.update_accelerations()
+        self.update_states()
+
+
+
+
+        
+
 
 class QuarterCar(DynamicObject):
     def __init__(self, body_mass = 500,
