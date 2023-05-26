a2e8fb2bb33080e524cc89c81cc7aa38272dd803
mhdbabayi <m.babayi-semiromi@cranfield.ac.uk>
2023-05-26 10:04:17 +0100

    regular commit

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -9,12 +9,12 @@ os.system("clear")
 forward_speed = 2.7
 
 road = flx.Road(
-                step_width=0.5,
-                step_height=0.5,
+                step_width=0.01,
+                step_height=0.08,
                 step_profile_phase=np.pi,
                 high_res=True
                 )
-tyre = flx.Tyre_Continous(initial_x=2.1,
+tyre = flx.Tyre_Continous(initial_x=1,
                 initial_y=0.34,
                 road=road,
                 free_radius=0.35,
@@ -22,7 +22,7 @@ tyre = flx.Tyre_Continous(initial_x=2.1,
                 y_speed=0,
                 )
 q_car = flx.SprungMass(tyre_inst=tyre,
-                       mass = 500,
+                       mass = 630,
                        speed_x=tyre.states.velocity.x,
                        speed_y=1,
                        spring_neutral_length=1,
@@ -31,7 +31,9 @@ q_car = flx.SprungMass(tyre_inst=tyre,
 qmain_fig = plt.figure()
 
 
-for i in range(500):    
+#for i in range(500): 
+logged_data = []   
+while tyre.states.position.x < 2.7:
     st = time.time() # For timing the main operations
     # main dynamics updates, should really be done in a function
     # but because of the way the external forces are implemented, it's done 
@@ -44,12 +46,18 @@ for i in range(500):
     tyre.draw()
     q_car.draw()
     plt.plot(road.x, road.y, color="black")
-    plt.gca().set_aspect('equal')
+    #plt.gca().set_aspect('equal')
     plt.draw()
     plt.xlim((tyre.states.position.x - tyre.free_radius*1.1,
               tyre.states.position.x + tyre.free_radius*1.5))
     plt.ylim((tyre.states.position.y - tyre.free_radius*1.1, 
               q_car.states.position.y + tyre.free_radius*1.1))
-    while not plt.waitforbuttonpress():
-        pass
+    #while not plt.waitforbuttonpress():
+    #    pass
+    logged_data.append([tyre.forces, tyre.states.position])
     plt.cla()
+print("finished")
+logged_forces = np.array([v[0] for v in logged_data])
+position = np.array([v[1] for v in logged_data])
+plt.plot(position[: , 0], logged_forces[: , 1])
+plt.show()
